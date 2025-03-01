f3874875c70b3a5a968a6f67d1f0ad632d71c6bd
Marquesxy <mairon.msantos@senacsp.edu.br>
2025-03-01 15:11:30 -0300

    feat: organizanção

--- AulaCEV/script.js
+++
@@ -1,9 +0,0 @@
-/*Ex1
-let nome = prompt("Qual é o seu nome?")
-alert("É um grande prazer em te conhecer, " + nome + "!")*/
-
-//Ex2
-let n1 = parseInt(prompt("Digite um número: "))
-let n2 = parseInt(prompt("Digite outro numero: "))
-let s = n1 + n2
-alert ("A soma dos Numero digitado é: " + s)
